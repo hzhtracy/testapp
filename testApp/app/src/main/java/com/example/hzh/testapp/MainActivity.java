fab2ed8d078a8a182359524514a860f460ac6df5
hzhtracy <hezhenghai@126.com>
2016-07-14 13:43:19 +0800

    update app

--- testApp/app/src/main/java/com/example/hzh/testapp/MainActivity.java
+++ testApp/app/src/main/java/com/example/hzh/testapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hzh.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv;
